98fe7de9174a0a6aa88f97bbd97051d85cc79370
IvanSemchuk <ivan7chuk@gmail.com>
2017-07-18 18:15:21 +0300

    Face detection in process

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -2,42 +2,63 @@ import cv2
 import asyncio
 
 
-def  camera_open(device_num):
-    v_cam = cv2.VideoCapture(device_num)
-    v_cam.open(device_num)
-    return v_cam
+class Image_processor:
 
-def camera_forever_loop(camera):
-    while 1:
-        bla, image = camera.read()
-        cv2.imshow("image", image)
+    def __init__(self, period):
+        self.face = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+        self.result_array = []
+        self.discrete_period = period
+        return
 
-        blue, green ,red = cv2.split(image)
-        get_image_sum(green)
-        cv2.waitKey(100)
+    def  camera_open(self, device_num):
+        v_cam = cv2.VideoCapture(device_num)
+        v_cam.open(device_num)
+        return v_cam
 
-def get_image_sum(image):
-    sum = cv2.sumElems(image)
-    return sum[0]
+    def get_image_sum(self, image):
+        sum = cv2.sumElems(image)
+        return sum[0]
+
+    def camera_forever_loop(self, camera):
+        while 1:
+            bla, image = camera.read()
+            cv2.imshow("image", image)
+            blue, green ,red = cv2.split(image)
+            self.get_image_sum(green)
+            cv2.waitKey(100)
+
+    def detect_face(self, image):
+        faces = self.face.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), maxSize=(250,250))
+        print(faces)
+        return faces
 
 
-
-result_array = []
-discrete_period = 1/20
+imProc = Image_processor(1)
+# cam = imProc.camera_open(0)
+# bla, image = cam.read()
+# face = imProc.detect_face(image)
+# print(len(face))
+#
+# cv2.rectangle(image, (face[0], face[1]),(face[2], face[3]))
 
 @asyncio.coroutine
 def periodic(camera):
-    while len(result_array)<4:
+    #while len(imProc.result_array)<4:
+     while True:
         bla, image = camera.read()
+        face = imProc.detect_face(image)
+        #cv2.rectangle(image, (face[0], face[1]), (face[2], face[3]))
+        cv2.imshow("image",image)
         blue, green, red = cv2.split(image)
-        result_array.append(get_image_sum(green))
-        yield from asyncio.sleep(discrete_period)
+        imProc.result_array.append(imProc.get_image_sum(green))
+        cv2.waitkey(10)
+        yield from asyncio.sleep(imProc.discrete_period)
 
 def stop():
     print("blea")
     task.cancel()
 
-cam = camera_open(0)
+cam = imProc.camera_open(0)
 task = asyncio.Task(periodic(cam))
 loop = asyncio.get_event_loop()
 
@@ -46,4 +67,4 @@ try:
 except asyncio.CancelledError as e:
     pass
 
-print(result_array)
+print(imProc.result_array)
