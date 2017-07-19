42c6ffdfe1d321056d86d2fde399a096a92627dd
SemVan <ivan7chuk@gmail.com>
2017-07-19 21:23:29 +0300

    Face detection is done

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -19,24 +19,33 @@ class Image_processor:
         sum = cv2.sumElems(image)
         return sum[0]
 
-    def camera_forever_loop(self, camera):
-        while 1:
-            bla, image = camera.read()
-            cv2.imshow("image", image)
-            blue, green ,red = cv2.split(image)
-            self.get_image_sum(green)
-            cv2.waitKey(100)
-
     def detect_face(self, image):
         faces = self.face.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), maxSize=(250,250))
-        print(faces)
-        return faces
+        if len(faces) > 0:
+            strt_x = faces[0][0]
+            strt_y = faces[0][1]
+            width = faces[0][2]
+            height = faces[0][3]
+            only_face = image[strt_y:strt_y+height, strt_x:strt_x+width]
+            return only_face
+        return image
 
 
-imProc = Image_processor(1)
+imProc = Image_processor(0.01)
 # cam = imProc.camera_open(0)
 # bla, image = cam.read()
 # face = imProc.detect_face(image)
+#
+# if len(face)>0:
+#     strt_x = face[0][0]
+#     strt_y = face[0][1]
+#     width = face[0][2]
+#     height = face[0][3]
+#     cv2.rectangle(image, (strt_x, strt_y),(strt_x+width, strt_y+height), (255,0,0), 2)
+#
+# cv2.imshow("image", image)
+# cv2.waitKey()
+# cam.close()
 # print(len(face))
 #
 # cv2.rectangle(image, (face[0], face[1]),(face[2], face[3]))
@@ -47,11 +56,10 @@ def periodic(camera):
      while True:
         bla, image = camera.read()
         face = imProc.detect_face(image)
-        #cv2.rectangle(image, (face[0], face[1]), (face[2], face[3]))
-        cv2.imshow("image",image)
-        blue, green, red = cv2.split(image)
-        imProc.result_array.append(imProc.get_image_sum(green))
-        cv2.waitkey(10)
+        cv2.imshow("image", face)
+        #blue, green, red = cv2.split(image)
+        #imProc.result_array.append(imProc.get_image_sum(green))
+        cv2.waitKey(1)
         yield from asyncio.sleep(imProc.discrete_period)
 
 def stop():
